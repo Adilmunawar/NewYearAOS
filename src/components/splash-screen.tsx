7cf934993f80a1807e204937f6c681909de24377
Adil Munawar <adilfromavengers@gmail.com>
2025-12-31 15:38:19 +0000

    I have added a gif in public folder named as server traffic use it inste

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import ServerTraffic from './server-traffic-svg';
+import Image from 'next/image';
 
 export default function SplashScreen() {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-background z-50 animate-fade-out" style={{ animationFillMode: 'forwards', animationDelay: '3.5s' }}>
-      <div className="w-64 h-64 md:w-80 md:h-80 animate-fade-in-up">
-        <ServerTraffic />
+      <div className="w-64 h-64 md:w-80 md:h-80 animate-fade-in-up relative">
+        <Image src="/server-traffic.gif" alt="Server traffic animation" layout="fill" objectFit="contain" unoptimized />
       </div>
       <p className="mt-8 text-white/80 text-lg md:text-xl font-semibold animate-fade-in-up" style={{ animationDelay: '0.2s' }}>
         Arry itne ziada log agaey site py
