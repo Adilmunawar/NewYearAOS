9798a5412af3ec06d0396e98d5f782c8246846fa
Adil Munawar <adilfromavengers@gmail.com>
2025-12-31 18:43:45 +0000

    when I types my name "adil munawar" it shows "ail munawar" and if I type

--- src/components/typewriter.tsx
+++ src/components/typewriter.tsx
@@ -15,7 +15,7 @@ const useTypewriter = (text: string, speed = 50) => {
     let i = 0;
     const typingInterval = setInterval(() => {
       if (i < text.length) {
-        setDisplayText(prevText => prevText + text.charAt(i));
+        setDisplayText(text.substring(0, i + 1));
         i++;
       } else {
         clearInterval(typingInterval);
